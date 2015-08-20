4030a8bcca0abcf7e0506ad0557088d8cd973c48
kgerov <kgerov@abv.bg>
2015-08-20 02:26:28 -0700

    find number of roots of graph

---
+++ 05.TreeAndGraphTraversal/05.TreeAndGraphTraversal/Graph.cs
@@ -0,0 +1,79 @@
+﻿namespace _05.TreeAndGraphTraversal
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Graph
+    {
+        private List<int>[] graph;
+        private bool[] isVisited;
+
+        public Graph(int numberOfNodes)
+        {
+            this.graph = new List<int>[numberOfNodes];
+            for (int i = 0; i < this.graph.Length; i++)
+            {
+                this.graph[i] = new List<int>();
+            }
+
+            this.isVisited = new bool[numberOfNodes];
+        }
+
+        public void AddChild(int parentNode, int childNode)
+        {
+            this.graph[parentNode].Add(childNode);
+        }
+
+        public int GetNumberOfRootNodes()
+        {
+            HashSet<int> childNodes = new HashSet<int>();
+
+            foreach (List<int> node in this.graph)
+            {
+                foreach (int child in node)
+                {
+                    childNodes.Add(child);
+                }
+            }
+
+            int parentCount = 0;
+
+            for (int node = 0; node < this.graph.Length; node++)
+            {
+                if (!childNodes.Contains(node))
+                {
+                    parentCount++;
+                }
+            }
+
+            return parentCount;
+        }
+
+        //for (int i = 0; i < this.isVisited.Length; i++)
+        //{
+        //    this.isVisited[i] = false;
+        //}
+
+        //for (int node = 0; node < this.graph.Length; node++)
+        //{
+        //    if (!this.isVisited[node])
+        //    {
+        //        DFS(node);
+        //        Console.WriteLine();
+        //    }
+        //}
+
+        private void DFS(int node)
+        {
+            if (!this.isVisited[node])
+            {
+                this.isVisited[node] = true;
+
+                foreach (int child in this.graph[node])
+                {
+                    DFS(child);
+                }
+            }
+        }
+    }
+}
---
+++ 05.TreeAndGraphTraversal/05.TreeAndGraphTraversal/Tester.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05.TreeAndGraphTraversal
+{
+    class Tester
+    {
+        static void Main()
+        {
+            int numberOfNodes = Int32.Parse(Console.ReadLine());
+            int numberOfEdges = Int32.Parse(Console.ReadLine());
+
+            Graph graph = new Graph(numberOfNodes);
+
+            for (int i = 0; i < numberOfEdges; i++)
+            {
+                string lineWithNodes = Console.ReadLine();
+                string[] stringNodes = lineWithNodes.Split(' ');
+
+                int currentParentNode = Int32.Parse(stringNodes[0]); // First token is awlays the parent
+                int currentChildNode = Int32.Parse(stringNodes[1]); // Second token is awlays the child
+                
+                graph.AddChild(currentParentNode, currentChildNode);
+            }
+
+            int parentCount = graph.GetNumberOfRootNodes();
+
+            switch (parentCount)
+            {
+                case 0:
+                    Console.WriteLine("No root nodes");
+                    break;
+                case 1:
+                    Console.WriteLine("One root");
+                    break;
+                default:
+                    Console.WriteLine("Multiple roots");
+                    break;
+            }
+
+            Main();
+        }
+    }
+}
