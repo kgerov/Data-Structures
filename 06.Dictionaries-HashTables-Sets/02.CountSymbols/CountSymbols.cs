0315af6d2a89313f557c94e8078090f813c63b41
kgerov <kgerov@abv.bg>
2015-09-04 18:56:28 +0300

    count symbols problem

---
+++ 06.Dictionaries-HashTables-Sets/02.CountSymbols/CountSymbols.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using DictionaryDataStructure;
+
+namespace _02.CountSymbols
+{
+    class CountSymbols
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> symbols = new Dictionary<char, int>();
+   
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = Convert.ToChar(text[i]);
+
+                if (!symbols.ContainsKey(text[i]))
+                {
+                    symbols[currentChar] = 0;
+                }
+
+                symbols[currentChar]++;
+            }
+
+            var orderedSymbols = symbols.ToList();
+            orderedSymbols.Sort(delegate(KeyValue<char, int> c1, KeyValue<char, int> c2) { return c1.Key.CompareTo(c2.Key); });
+
+            foreach (var symbol in orderedSymbols)
+            {
+                Console.WriteLine(symbol + " time/s");
+            }
+        }
+    }
+}
