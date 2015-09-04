769ce4312564eb4a2f821e625c81a23150432b94
kgerov <kgerov@abv.bg>
2015-09-04 19:21:57 +0300

    phonebook problem

---
+++ 06.Dictionaries-HashTables-Sets/03.Phonebook/Phonebook.cs
@@ -0,0 +1,58 @@
+﻿using DDS = DictionaryDataStructure;
+using System;
+using System.Collections.Generic;
+
+namespace _03.Phonebook
+{
+    class Phonebook
+    {
+        private const string SearchKeyWord = "search";
+
+        static void Main()
+        {
+            List<string> searchList = new List<string>();
+            DDS.Dictionary<string, string> contacts = new DDS.Dictionary<string, string>();
+            string inputLine = Console.ReadLine();
+            bool shouldAddContactsToSearch = false;
+
+            while (inputLine != "")
+            {
+                if (inputLine == SearchKeyWord)
+                {
+                    shouldAddContactsToSearch = true;
+                }
+                else
+                {
+                    if (shouldAddContactsToSearch)
+                    {
+                        searchList.Add(inputLine);
+                    }
+                    else
+                    {
+                        string[] tokens = inputLine.Split('-');
+                        string name = tokens[0]; // contact name always comes first
+                        string phoneNumber = tokens[1]; // phone number always comes second
+
+                        contacts.Add(name, phoneNumber);
+                    }
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var contactName in searchList)
+            {
+                var contact = contacts.Find(contactName);
+
+                if (contact != null)
+                {
+                    Console.WriteLine(contact);
+                }
+                else
+                {
+                    Console.WriteLine("Contact {0} does not exist.", contactName);
+                }
+            }
+        }
+    }
+}
