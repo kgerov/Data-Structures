b83c100187ffaaff29fe11b6460bdc3ff40014a1
kgerov <kgerov@abv.bg>
2015-09-12 14:39:20 +0300

    students by course task

---
+++ DataStructureEfficiency/01.StudentsAndCourses/Helpers/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01.StudentsAndCourses.Helpers
+{
+    public class Student : IComparable<Student>
+    {
+        public Student(string firstName, string lastName, string course)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Course = course;
+        }
+
+        public Student()
+        {
+            
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Course { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            string thisReversedName = this.LastName + this.FirstName;
+            string otherReversedName = other.LastName + other.FirstName;
+
+            return thisReversedName.CompareTo(otherReversedName);
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ DataStructureEfficiency/01.StudentsAndCourses/StudentsAndCourses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using _01.StudentsAndCourses.Helpers;
+
+namespace _01.StudentsAndCourses
+{
+    class StudentsAndCourses
+    {
+        static void Main()
+        {
+            var studentsByCourse = new SortedDictionary<string, SortedSet<Student>>();
+
+            using (StreamReader streamReader = new StreamReader("../../InputFile/Students.txt"))
+            {
+                string line;
+
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    string[] tokens = line.Split('|');
+                    string firstName = tokens[0].Trim();
+                    string lastName = tokens[1].Trim();
+                    string course = tokens[2].Trim();
+
+                    Student currentStudent = new Student(firstName, lastName, course);
+
+                    if (!studentsByCourse.ContainsKey(course))
+                    {
+                        studentsByCourse.Add(course, new SortedSet<Student>());
+                    }
+
+                    studentsByCourse[course].Add(currentStudent);
+                }
+            }
+
+            foreach (var course in studentsByCourse)
+            {
+                Console.WriteLine(course.Key + ": " + String.Join(", ", course.Value));
+            }
+        }
+    }
+}
